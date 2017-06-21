a894870fff1adc4eb8d542382e60b67ed1401351
Ismaïl <nguyen.ismail@gmail.com>
2017-06-21 16:07:52 +0200

    Add session to store access token

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './components/App.vue'
 import VueRouter from 'vue-router'
 import { firebaseApp } from './firebaseApp' 
+import VueSession from 'vue-session'
 
+Vue.use(VueSession)
 Vue.use(VueRouter)
 
 import store from './store'
 
+import Auth from './components/Auth.vue';
 import Dashboard from './components/Dashboard.vue'
 import Signin from './components/Signin.vue'
 import Signup from './components/Signup.vue'
@@ -16,7 +19,8 @@ const router = new VueRouter({
     routes: [
         { path: '/dashboard', component: Dashboard },
         { path: '/signin', component: Signin },
-        { path: '/signup', component: Signup }
+        { path: '/signup', component: Signup },
+        { path: '/Auth', component: Auth}
     ]
 })
 
@@ -26,7 +30,7 @@ firebaseApp.auth().onAuthStateChanged(user => {
         router.push('/dashboard')
     }
     else {
-        router.replace('/signin')
+        router.replace('/auth')
     }
 })
 
